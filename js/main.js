e155a133fe193f2cd50675f0793de408abb78eaa
Edvvard <kamilmathers@gmail.com>
2022-11-16 02:31:38 +0200

    Added navigation

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const nav = document.querySelector('.nav__items')
+const burgerBtn = document.querySelector('.burger-btn')
+const navItems = document.querySelectorAll('.nav__item')
+
+const addNavItemsAnimation = () => {
+	let delayTime = 0
+
+	navItems.forEach(item => {
+		item.classList.add('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
+}
+
+const deleteNavItemsAnimation = () => {
+	navItems.forEach(item => {
+		item.classList.remove('nav-items-animation')
+	})
+}
+
+const handleNav = () => {
+	nav.classList.toggle('nav__items--active')
+	addNavItemsAnimation()
+	navItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav__items--active')
+			deleteNavItemsAnimation()
+		})
+	})
+}
+burgerBtn.addEventListener('click', handleNav)
